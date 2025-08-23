bd32809f66f0a1d6c2dc6aea05bf8d50ad2c6723
cnerylozada <cnerylozada@gmail.com>
2025-08-23 18:47:07 -0500

    chore: in progress

--- app/regenerative_actions/_components/ActionList.tsx
+++ app/regenerative_actions/_components/ActionList.tsx
@@ -1,15 +1,31 @@
 "use client";
 
+import { getDecryptedData } from "@/server/manageActions";
 import { useEffect, useState } from "react";
 import { useActiveAccount } from "thirdweb/react";
+import { IRegenerativeAction } from "./models";
 
 export const ActionList = () => {
   const activeAccount = useActiveAccount();
-  const [farmerData, setFarmerData] = useState();
+  const [actions, setActions] = useState<IRegenerativeAction[] | undefined>(
+    undefined
+  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (activeAccount) {
+      setIsLoading(true);
+      getDecryptedData(activeAccount.address).then((_) => {
+        setActions(_);
+        setIsLoading(false);
+      });
+    }
+  }, [activeAccount]);
 
   return (
     <div>
-      <div>ActionList</div>
+      <div>{isLoading && "Decrypting actions ..."}</div>
+      <div>{JSON.stringify(actions)}</div>
     </div>
   );
 };
